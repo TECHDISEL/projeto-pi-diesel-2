a2014518a3dd43af22da8812ef2c66bfe96f29d6
ErickHlee <erick.hlee@sptech.school>
2024-09-22 22:08:54 -0300

    Update calculadora

--- Calculadoras/Simulador sprint2/script.js
+++ Calculadoras/Simulador sprint2/script.js
@@ -21,10 +21,10 @@ function calcular_diesel_tanque_deitado(){
     var volume_diesel_litros = volume_diesel * 1000
 
     resultado_volume_diesel.innerHTML = `
-    O volume atual do Diesel é de (aproximadamente): <b>${Math.floor(volume_diesel_litros)}L</b>.
+    O volume atual do Diesel é de (aproximadamente): <b>${(Math.floor(volume_diesel_litros).toLocaleString('pt-BR'))}L</b>.
     <br><br>
     É importante delimitar um volume mínimo de estoque do Diesel, para que seu negócio não tenha quebra de fluxo. <br>
-    De acordo com o nosso cálculo, em cima dos dados informados, é possível dizer que o volume mínimo que você deve manter no seu tanque é de <b>${dias_reabastecimento * diesel_utilizado_por_dia}L</b>.
+    De acordo com o nosso cálculo, em cima dos dados informados, é possível dizer que o volume mínimo que você deve manter no seu tanque é de <b>${(dias_reabastecimento * diesel_utilizado_por_dia).toLocaleString('pt-BR')}L</b>.
     <br><br>
     Com o nosso projeto, é possível realizar esse monitoramento ao vivo. O sensor HC-SR04, sensor ultrassônico que mede distância, irá medir a distância do ponto mais alto do tanque até a altura atual do Diesel em relação ao tanque, permitindo realizar o cálculo do volume do combustível. 
     <br>
@@ -44,10 +44,10 @@ function calcular_diesel_tanque_em_pe(){
     var volume_diesel_litros = volume_diesel * 1000
 
     resultado_volume_diesel.innerHTML = `
-    O volume atual do Diesel é de (aproximadamente): <b>${Math.floor(volume_diesel_litros)}L</b>.
+    O volume atual do Diesel é de (aproximadamente): <b>${(Math.floor(volume_diesel_litros).toLocaleString('pt-BR'))}L</b>.
     <br><br>
     É importante delimitar um volume mínimo de estoque do Diesel, para que seu negócio não tenha quebra de fluxo. <br>
-    De acordo com o nosso cálculo, em cima dos dados informados, é possível dizer que o volume mínimo que você deve manter no seu tanque é de <b>${dias_reabastecimento * diesel_utilizado_por_dia}L</b>.
+    De acordo com o nosso cálculo, em cima dos dados informados, é possível dizer que o volume mínimo que você deve manter no seu tanque é de <b>${(dias_reabastecimento * diesel_utilizado_por_dia.toLocaleString('pt-BR'))}L</b>.
     <br><br>
     Com o nosso projeto, é possível realizar esse monitoramento ao vivo. O sensor HC-SR04, sensor ultrassônico que mede distância, irá medir a distância do ponto mais alto do tanque até a altura atual do Diesel em relação ao tanque, permitindo realizar o cálculo do volume do combustível. 
     <br>
