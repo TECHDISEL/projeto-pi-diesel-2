31f54dce1a1859fbcf39e484b88273750bc880db
ArielCSousa <ariel.sousa@sptech.school>
2024-09-28 14:17:09 -0300

    correção de modelagem das tabelas e do script no MySQL

--- tabelasBD/Tabelas Sprint 2/Tabelasnovas-3tabelas-sprint2-seminserts.sql
+++ tabelasBD/Tabelas Sprint 2/Tabelasnovas-3tabelas-sprint2-seminserts.sql
@@ -7,24 +7,24 @@ nome varchar(40) not null,
 telefone varchar(11),
 endereco varchar(30),
 responsavel varchar(15),
-email varchar(30)
+email varchar(30) not null,
+senha varchar(45) not null
 );
 
 create table sensor(
 idsensor int primary key auto_increment,
 nivel varchar(10),
-dataehora datetime not null,
-fkCliente int,
-constraint fksensorcliente foreign key (fkCliente)
-	references cliente(idCliente)
+dataehora datetime not null
 );
 
 create table tanque(
 idTanque int primary key auto_increment,
 capacidadeLitros decimal(10,2),
-tipoTanque varchar(10) not null,
-CONSTRAINT chkTipoTanque CHECK (tipotanque IN ('Vertical', 'Horizontal')),
+fkSensor int,
 fkCliente int,
-constraint fktanquecliente foreign key (fkCliente)
+constraint fktanquesensor foreign key (fkSensor)
+	references sensor(idSensor),
+constraint fkclientetanque foreign key (fkCliente)
 	references cliente(idCliente)
 );
+
