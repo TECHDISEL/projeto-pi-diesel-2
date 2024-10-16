1a40465a0cf72d0cd572ea17b1dd15712d8cb2a4
Guilherme-Gotardo <guilherme.santos043@sptech.school>
2024-10-15 23:04:17 -0300

    Atualização nas dash + lógica de login

--- API/dat-acqu-ino/main.js
+++ API/dat-acqu-ino/main.js
@@ -63,10 +63,6 @@ const serial = async (valoresSensorDigital) => {
             );
 
             console.log("valores inseridos no banco: ", sensorDigital);
-<<<<<<< HEAD
-
-=======
->>>>>>> e5693ab3fc105b0c2356d0fc028a98a5bf837c7a
         }
 
     });
