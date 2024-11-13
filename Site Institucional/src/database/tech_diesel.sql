96e112c61514e330c9fb23c7ccd7fddc96559374
Guilherme-Gotardo <guilherme.santos043@sptech.school>
2024-11-12 21:40:44 -0300

    Inicio da API em ambiente de teste

---
+++ Site Institucional/src/database/tech_diesel.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE tech_diesel;
+USE tech_diesel;
+
+/*TABELA DE CADASTRO DA EMPRESA*/
+CREATE TABLE cadastro (
+CNPJ CHAR(18) PRIMARY KEY,
+nome VARCHAR(45),
+nomeFantasia VARCHAR(45),
+razaoSocial VARCHAR(45),
+telefone CHAR(11),
+email VARCHAR(45),
+senha VARCHAR(45)
+);
+
+/*  TABELAS PARA USUÁRIO E CADASTRO  */
+CREATE TABLE usuario (
+idUsuario INT,
+fkCNPJ CHAR(18),
+fkResponsavel INT,
+nome VARCHAR(45),
+senha VARCHAR(45),
+
+CONSTRAINT pkCompostaUsuario PRIMARY KEY (idUsuario, fkCNPJ),
+CONSTRAINT fkUsuarioResponsavel FOREIGN KEY (fkResponsavel) REFERENCES usuario (idUsuario)
+);
+
+/* TABELA PARA ENDEREÇO*/
+CREATE TABLE endereco (
+idEndereco INT,
+fkCNPJ CHAR(18) UNIQUE,
+rua VARCHAR(45),
+bairro VARCHAR(45),
+numero INT,
+CEP CHAR(8),
+cidade VARCHAR(45),
+uf CHAR(2),
+
+CONSTRAINT pkCompostaEndereco PRIMARY KEY (idEndereco, fkCNPJ),
+CONSTRAINT fkCNPJEndereco FOREIGN KEY (fkCNPJ) REFERENCES cadastro (CNPJ)
+);
+
+/* TABELA PARA SENSOR*/
+CREATE TABLE sensor (
+idSensor INT PRIMARY KEY AUTO_INCREMENT, 
+dataInstalacao DATE
+);
+
+/*  TABELAS PARA TANQUE*/
+CREATE TABLE tanque (
+idTanque INT PRIMARY KEY AUTO_INCREMENT,
+fkCadastro CHAR(18),
+fkSensor INT,
+nome VARCHAR(45),
+capacidade INT,
+
+CONSTRAINT fkCadastroTanque FOREIGN KEY (fkCadastro) REFERENCES cadastro (CNPJ),
+CONSTRAINT fkSensorTanque FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
+);
+
+/*  TABELAS PARA MEDIDA*/
+CREATE TABLE medida (
+idMedida INT PRIMARY KEY AUTO_INCREMENT,
+fkSensor INT,
+leitura DECIMAL(12,2),
+dataLeitura DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+CONSTRAINT fkMedidaSensor FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
+);
