578b34c92c1f9aa36fe918e72de06fe6aeebdd48
VitorSuave <vitor.rodrigues@sptech.school>
2024-11-13 16:42:03 -0300

    Autenticação Login (Pendente)

--- Site Institucional/src/models/usuarioModel.js
+++ Site Institucional/src/models/usuarioModel.js
@@ -1,13 +1,13 @@
 var database = require("../database/config")
 
-// function autenticar(email, senha) {
-//     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
-//     var instrucaoSql = `
-//         SELECT id, nome, email, fk_empresa as empresaId, cpf FROM usuario WHERE email = '${email}' AND senha = '${senha}';
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucaoSql = `
+        SELECT cnpj, nome, email, senha FROM cadastro WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
 function cadastrar(cnpj, nome, razao, tel, email, senha) {
@@ -23,5 +23,6 @@ function cadastrar(cnpj, nome, razao, tel, email, senha) {
 }
 
 module.exports = {
-    cadastrar
+    cadastrar,
+    autenticar
 };
