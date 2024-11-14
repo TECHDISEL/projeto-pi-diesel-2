05321cba483f0b59521e17aad661185f82e964c6
Guilherme-Gotardo <guilherme.santos043@sptech.school>
2024-11-13 23:48:43 -0300

    Atualização rotas empresas

--- Site Institucional/public/js/scriptLogin.js
+++ Site Institucional/public/js/scriptLogin.js
@@ -257,3 +257,71 @@ function cadastrarEmpresa() {
     return false;
 
 }
+
+function entrar() {
+
+    var emailVar = email_login.value;
+    var senhaVar = senha_login.value;
+
+    if (emailVar == "" || senhaVar == "") {
+        cardErro.style.display = "block"
+        mensagem_erro.innerHTML = "(Mensagem de erro para todos os campos em branco)";
+
+    }
+    else {
+        setInterval(sumirMensagem, 5000)
+    }
+
+    console.log("FORM LOGIN: ", emailVar);
+    console.log("FORM SENHA: ", senhaVar);
+
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailVar,
+            senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log('AAAAAAAAAAAAAAAAAA')
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.NOME_USUARIO = json.nome;
+                sessionStorage.CPF_USUARIO = json.cpf;
+                sessionStorage.ID_USUARIO = json.id;
+                sessionStorage.TANQUES = JSON.stringify(json.tanques)
+
+                setTimeout(function () {
+                    window.location = "./dashboard.html";
+                }, 1000); // apenas para exibir o loading
+
+            });
+
+        } else {
+
+            console.log("Houve um erro ao tentar realizar o login!");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
+
+function sumirMensagem() {
+    cardErro.style.display = "none"
+}
