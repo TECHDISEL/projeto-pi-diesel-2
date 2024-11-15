6b2ceb623df567597ac5c03af76e101fedac904d
Guilherme-Gotardo <guilherme.santos043@sptech.school>
2024-11-15 18:49:19 -0300

    Traçando rotas para seleção de tanques

--- Site Institucional/src/models/tanqueModel.js
+++ Site Institucional/src/models/tanqueModel.js
@@ -8,7 +8,18 @@ function buscarTanquesPorEmpresa(idEmpresa) {
   return database.executar(instrucaoSql);
 }
 
+function retornarTanque(idEmpresa) {
+
+  var instrucaoSql = `SELECT * FROM tanque WHERE idTanque = ?;`
+  [req.params.idTanque];
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+
+}
+
 
 module.exports = {
   buscarTanquesPorEmpresa,
+  retornarTanque
 }
