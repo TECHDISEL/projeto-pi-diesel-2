af942feaa6cb3ebdaefb318a6399651d85890cb3
Guilherme-Gotardo <guilherme.santos043@sptech.school>
2024-11-16 16:05:40 -0300

    att

--- tabelasBD/Tabelas Sprint 2/scriptsV2.sql
+++ tabelasBD/Tabelas Sprint 2/scriptsV2.sql
@@ -3,7 +3,9 @@
 CREATE DATABASE tech_diesel;
 USE tech_diesel;
 
-/*TABELA DE CADASTRO DA EMPRESA*/
+/* =================================================
+		   TABELA DE CADASTRO DA EMPRESA
+================================================= */
 CREATE TABLE empresa (
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 CNPJ CHAR(18),
@@ -14,18 +16,18 @@ codigo_ativacao CHAR(8)
 );
 
 INSERT INTO Empresa (idEmpresa,CNPJ,nomeFantasia, razaoSocial, telefone, codigo_ativacao) VALUES 
+(DEFAULT, '000-000', 'Admin TECHDIESEL', 'TECHDIESEL INC', '123456', 'TCHDL999'), -- CADASTRO DE EMPRESA PARA TESTES INTERNOS
 (DEFAULT,'12.345.678/0001-90', 'Fazendas Frizza', 'Agro & Comércio Ltda', '12345678901', 'TCHDL001');
 
-INSERT INTO Empresa (idEmpresa,CNPJ,nomeFantasia, razaoSocial, telefone, codigo_ativacao) VALUES 
-(DEFAULT, '000-000', 'Admin TECHDIESEL', 'TECHDIESEL INC', '123456', 'TCHDL999');
-
-/*  TABELAS PARA USUÁRIO E CADASTRO  */
+/* =================================================
+		 TABELAS PARA USUÁRIO E CADASTRO
+================================================= */
 CREATE TABLE usuario (
 idUsuario INT AUTO_INCREMENT,
 fkEmpresa INT,
 fkResponsavel INT,
 nome VARCHAR(45),
-email VARCHAR(45),
+email VARCHAR(45) UNIQUE,
 senha VARCHAR(45),
 cargo VARCHAR(45),
 
@@ -34,16 +36,9 @@ CONSTRAINT fkUsuarioEmpresa foreign key (fkEmpresa) REFERENCES empresa(idEmpresa
 CONSTRAINT fkUsuarioResponsavel FOREIGN KEY (fkResponsavel) REFERENCES usuario (idUsuario)
 );
 
-ALTER TABLE usuario MODIFY COLUMN email VARCHAR(45) UNIQUE;
-SELECT * FROM usuario;
-
-/*
-INSERT INTO usuario (idUsuario, fkEmpresa, fkResponsavel, nome, email, senha, cargo) VALUES 
-(1, 1, 1,'Admin', 'admin@admin.com', 'Admin123@', 'Admin'),
-(2, 1, 1, 'Frizza', 'frizza@example.com', 'Fr@123', 'Dono'),     
-(3, 1, 1, 'Julia', 'julia@example.com', 'Ju@123', 'Supervisora'); */
-
-/* TABELA PARA ENDEREÇO*/
+/* =================================================
+TABELA PARA ENDEREÇO (SERÃO CADASTRADOS PELA PRÓPRIA TECH DIESEL, VAMOS DISPONIBILIZAR QUE OS USUÁRIOS FAÇAM ALTERAÇÕES)
+================================================= */
 CREATE TABLE endereco (
 idEndereco INT AUTO_INCREMENT,
 fkEmpresa INT,
@@ -61,18 +56,14 @@ CONSTRAINT fkEmpresaEndereco FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpre
 INSERT INTO endereco (idEndereco, fkEmpresa, rua, bairro, numero, CEP, cidade, uf) VALUES 
 (1, 1, 'Rua das Flores', 'Centro', 123, '01234567', 'São Paulo', 'SP');
 
-/* TABELA PARA SENSOR*/
+/* =========================================================
+TABELAS SENSOR, TANQUES E MEDIDAS (MANTIDAS INTERNAMENTE)
+========================================================= */
 CREATE TABLE sensor (
 idSensor INT PRIMARY KEY AUTO_INCREMENT, 
-dataInstalacao DATE
+dataInstalacao DATETIME
 );
 
-INSERT INTO sensor VALUES 
-(DEFAULT, '2024-10-31'),
-(DEFAULT, '2024-11-01'),
-(DEFAULT, '2024-11-01');
-
-/*  TABELAS PARA TANQUE*/
 CREATE TABLE tanque (
 idTanque INT PRIMARY KEY AUTO_INCREMENT,
 fkEmpresa INT,
@@ -85,14 +76,19 @@ CONSTRAINT fkEmpresaTanque FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa
 CONSTRAINT fkSensorTanque FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
 );
 
-INSERT INTO tanque (fkEmpresa, fkSensor, setor, alturaMetro, raio) VALUES
-(1, 1, 'Irrigação', '4', '2.0'),
-(1, 1, 'Abastecimento Tratores', '3', '2.2'),
-(1, 1, 'Abastecimento Maquinario Pesado', '3', '2.2');
+INSERT INTO sensor (idSensor, dataInstalacao) VALUES 
+(1, '2024-11-16 15:30:00'),
+(2, '2024-11-16 16:00:30');
 
-/*  TABELAS PARA MEDIDA*/
+INSERT INTO tanque (idTanque, fkEmpresa, fkSensor, setor, alturaMetro, raio) VALUES
+(1, 1, 1, 'Maquinario', '4', '1.7'), -- 35000 L
+(2, 1, 2, 'Irrigação', '2', '1.5'); -- 15000 L
+
+/* =========================================================
+TABELAS PARA CONTROLES DAS MEDIDAS E ALERTAS DOS SENSORES
+========================================================= */
 CREATE TABLE medida (
-idMedida INT,
+idMedida INT AUTO_INCREMENT,
 fkSensor INT,
 leitura DECIMAL(6,2),
 dataLeitura DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -111,7 +107,19 @@ CONSTRAINT pkAlertaMedida PRIMARY KEY (idAlerta, fkMedida),
 CONSTRAINT  fkAlertaMedida FOREIGN KEY (fkMedida) REFERENCES medida(idMedida)
 );
 
+ -- SELECT PARA CAPTAR MEDIDAS EM TEMPO REAL
+SELECT 
+        round(PI() * POW(tanque.raio, 2) * (tanque.alturaMetro - (medida.leitura/100)),0) * 1000 AS leitura,
+        medida.fkSensor,
+        dataLeitura,
+        DATE_FORMAT(dataLeitura,'%H:%i:%s') as dataLeitura
+FROM medida
+JOIN sensor ON medida.fkSensor = sensor.idSensor
+JOIN tanque ON sensor.idSensor = tanque.fkSensor
+WHERE medida.fkSensor = 1
+ORDER BY medida.idMedida DESC LIMIT 7;
 
+-- SELECT PARA VISUALIZAÇÃO DE TODOS OS DADOS CADASTRADOS
 SELECT 
     tanque.idTanque,
     tanque.nome AS tanque_nome,
@@ -160,5 +168,3 @@ LEFT JOIN
     sensor ON tanque.fkSensor = sensor.idSensor
 LEFT JOIN 
     medida ON sensor.idSensor = medida.fkSensor;
-    
-SELECT*FROM medida;
