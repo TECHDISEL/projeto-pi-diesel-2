f5fc4bcb55aca17baa4cb427cc0fdb4e4a31b2af
Guilherme-Gotardo <guilherme.santos043@sptech.school>
2024-12-04 17:19:23 -0300

    Arrumando login

--- Site Institucional/src/models/tanqueModel.js
+++ Site Institucional/src/models/tanqueModel.js
@@ -2,8 +2,7 @@ var database = require("../database/config");
 
 function buscarTanquesPorEmpresa(idEmpresa) {
 
-  var instrucaoSql = `SELECT * FROM tanque JOIN metricas ON idTanque = fkTanque 
-WHERE fkEmpresa = ${idEmpresa}`;
+  var instrucaoSql = `SELECT * FROM tanque WHERE fkEmpresa = ${idEmpresa}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
